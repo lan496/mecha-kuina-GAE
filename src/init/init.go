1a49c2ba0a5394550695ee58511eb626c45b7957
lan496 <kohei19950508@gmail.com>
2017-08-28 21:30:08 +0900

    store data as .json

--- src/init/init.go
+++ src/init/init.go
@@ -1,24 +1,45 @@
 package main
 
 import (
+	"encoding/json"
+	_ "fmt"
 	"github.com/lan496/mecha-kuina/src/update"
 	"log"
 	"os"
 )
 
 func main() {
-	idFile, err := os.Create("data/latestId.csv")
+	idFile, err := os.Create("data/latestId.json")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer idFile.Close()
 
 	var tweetsFile *os.File
-	tweetsFile, err = os.Create("data/tweets.csv")
+	tweetsFile, err = os.Create("data/tweets.json")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer tweetsFile.Close()
 
-	update.StoreTweetsAndLatestId(tweetsFile, idFile)
+	var dictFile *os.File
+	dictFile, err = os.Create("data/dict.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer dictFile.Close()
+
+	ss, _ := update.StoreTweetsAndLatestId(tweetsFile, idFile, 12345)
+
+	ms := make(map[string][]string)
+	for _, tw := range ss {
+		update.UpdateMarkovSpace(ms, tw)
+	}
+	var dict []byte
+	dict, err = json.Marshal(ms)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dictFile.Write(dict)
 }
--- src/update/update.go
+++ src/update/update.go
@@ -1,31 +1,44 @@
 package update
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/ChimeraCoder/anaconda"
 	"github.com/lan496/mecha-kuina/src/secret"
 	"github.com/lan496/mecha-kuina/src/twitter"
 	"os"
 	"strconv"
-	"strings"
 )
 
-func StoreTweetsAndLatestId(twf, idf *os.File) {
+func StoreTweetsAndLatestId(twf, idf *os.File, sinceId int64) (ss [][]string, latestId int64) {
 	anaconda.SetConsumerKey(secret.ConsumerKey)
 	anaconda.SetConsumerSecret(secret.ConsumerSecret)
 	api := anaconda.NewTwitterApi(secret.AccessToken, secret.AccessTokenSecret)
 
-	tweets, latestId, _ := twitter.FetchTweets(api, secret.Username, 2000, 12345)
+	var tweets []string
+	tweets, latestId, _ = twitter.FetchTweets(api, secret.Username, 2000, sinceId)
 
 	fmt.Println("fetched tweets:", len(tweets))
 	fmt.Println("latestId:", latestId)
 
-	idf.WriteString(strconv.FormatInt(latestId, 10))
+	tmpid, _ := json.Marshal(strconv.FormatInt(latestId, 10))
+	idf.Write(tmpid)
 
 	for _, tw := range tweets {
 		surfaces := twitter.Tokenize(twitter.TrimURL(tw))
-		storedStr := strings.Join(surfaces, " ") + "\n"
-		fmt.Println(storedStr)
-		twf.WriteString(storedStr)
+		fmt.Println(surfaces)
+		ss = append(ss, surfaces)
+	}
+	tmpss, _ := json.Marshal(ss)
+	twf.Write(tmpss)
+
+	return
+}
+
+func UpdateMarkovSpace(ms map[string][]string, surfaces []string) {
+	for i := 0; i < len(surfaces)-3; i++ {
+		key1 := surfaces[i] + "^" + surfaces[i+1]
+		key2 := surfaces[i+1] + "^" + surfaces[i+2]
+		ms[key1] = append(ms[key1], key2)
 	}
 }
